e6ede86af6a9d7f91e07a9b461851c7bac03e7b5
porter2003 <156115900+porter2003@users.noreply.github.com>
2025-02-24 14:47:10 -0700

    Added AddTask.cshtml file, Edited layout.cshtml and HomeController

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace Mission08Group4_7.Controllers
         {
             return View();
         }
+        public IActionResult AddTask()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
